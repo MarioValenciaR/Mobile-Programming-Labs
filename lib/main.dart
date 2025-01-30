8a74d9e434536db291eabded09628821e263da41
Mario Valencia <iaivalencia@hotmail.com>
2025-01-29 22:05:11 -0500

    Lab3 created

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab 2 Login Page',
+      title: 'Lab 2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
@@ -31,16 +31,16 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  String _imageSource = "images/question-mark.jpg"; // Imagen inicial
+  String _imageSource = "images/question-mark.jpg"; // Initial image
 
   void _onLoginPressed() {
     final password = _passwordController.text;
     setState(() {
       if (password == "QWERTY123") {
-        // Cambiar a la imagen de la bombilla
+        // Change to the light bulb image
         _imageSource = "images/light-bulb.jpg";
       } else {
-        // Cambiar a la imagen de la señal de alto
+        // Change to the stop sign image
         _imageSource = "images/stop-sign.jpg";
       }
     });
@@ -58,7 +58,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Campo de texto para el login
+            // Text field for login
             TextField(
               controller: _loginController,
               decoration: const InputDecoration(
@@ -66,24 +66,24 @@ class _LoginPageState extends State<LoginPage> {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 16.0), // Espaciado
-            // Campo de texto para la contraseña
+            const SizedBox(height: 16.0), // Spacing
+            // Text field for password
             TextField(
               controller: _passwordController,
-              obscureText: true, // Ocultar texto
+              obscureText: true, // Hide text
               decoration: const InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 20.0), // Espaciado
-            // Botón para iniciar sesión
+            const SizedBox(height: 20.0), // Spacing
+            // Button to log in
             ElevatedButton(
               onPressed: _onLoginPressed,
               child: const Text('Login'),
             ),
-            const SizedBox(height: 20.0), // Espaciado
-            // Imagen dinámica cargada localmente
+            const SizedBox(height: 20.0), // Spacing
+            // Dynamically loaded image
             Image.asset(
               _imageSource,
               width: 300,
